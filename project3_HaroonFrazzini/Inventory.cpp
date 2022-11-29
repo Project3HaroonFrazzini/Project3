c0fdd43db1f87a5312174f8a75ff4b0d4550a886
Vince Frazzini <vincefrazzini@Vinces-MacBook-Pro.local>
2022-11-29 13:37:38 -0700

    Inv

--- project3_HaroonFrazzini/Inventory.cpp
+++ project3_HaroonFrazzini/Inventory.cpp
@@ -34,7 +34,7 @@ void Inventory::printInventory(){
     string menu = "|-------------|\n|  INVENTORY  |\n|-------------|";
     string gold_print = "|  Gold       | " + to_string(gold);
     string cookware =   "|  Cookware   | P: " + to_string(ceramic_pot) + " |  F: " + to_string(frying_pan) + " |  C: " + to_string(couldron) + " |";
-    string weapon =     "|  Weapons    | C: " + to_string(club) + " |  S: " + to_string(spear) + " |  R: " + to_string(rapier) + " |";
+    string weapon =     "|  Weapons    | C: " + to_string(weapons[0]) + " |  S: " + to_string(weapons[1]) + " |  R: " + to_string(weapons[2]) + " | B: " + to_string(weapons[3]) + " | L: " + to_string(weapons[4]) + " |";
     string armor_print ="|  Armor      | " + to_string(armor);
     string treasures =  "|  Treasures  | R: " ; //to_string(treasure[0]) + "";
     cout << menu << endl << gold_print << endl << cookware << endl << weapon << endl << armor_print << endl;
