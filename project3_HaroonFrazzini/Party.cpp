2a03c1f5abee838e8e2edcb6997f7487edc66da8
Ali Haroon <aharoon3737@gmail.com>
2022-12-01 02:31:27 -0700

    Iwannadie

--- project3_HaroonFrazzini/Party.cpp
+++ project3_HaroonFrazzini/Party.cpp
@@ -56,49 +56,49 @@ int Party:: setFullness(int newFullness, int index)
 }
 void Party:: removeName(int index)
 {
-    names.erase(index);
+    names.erase(names.begin()+index);
 }
 void Party:: removeFullness(int index)
 {
-    fullnessValues.erase(index);
+    fullnessValues.erase(fullnessValues.begin()+index);
 }
 void Party:: deathFunc(int index)
 {
-    cout << "Oh no, party member" << getName(i) << " died!!!!" << endl;
-    removeName(i);
-    removeFullness(i);
-    inv.removeLives(i);
+    cout << "Oh no, party member" << getName(index) << " died!!!!" << endl;
+    removeName(index);
+    removeFullness(index);
+    inv.removeLives(index);
 }
 Inventory Party:: Misfortunes()
 {
     srand(time(0));
-    int rand = rand()%100 + 1;
-    if(rand <= 40)
+    int randum = rand()%100 + 1;
+    if(randum <= 40)
     {
-        rand = rand()%100 + 1;
-        if(rand <= 30)
+        randum = rand()%100 + 1;
+        if(randum <= 30)
         {
             cout << "OH NO! Your team was robbed by dungeon bandits!" << endl;
-            rand = rand()%3+ 1;
-            if(rand == 1)
+            randum = rand()%3+ 1;
+            if(randum == 1)
             {
                 cout << "You lost 10 ingredients!!!!" << endl;
                 inv.setIngredients(inv.getIngredients()-10);
             }
-            else if(rand == 2)
+            else if(randum == 2)
             {
-                rand = rand()%3 + 1;
-                if(rand == 1)
+                randum = rand()%3 + 1;
+                if(randum == 1)
                 {
-                    inv.setCookware(getCookWare(0)-1,0);
+                    inv.setCookware(inv.getCookware(0)-1,0);
                 }
-                else if(rand == 2)
+                else if(randum == 2)
                 {
-                    inv.setCookware(getCookWare(1)-1,1);
+                    inv.setCookware(inv.getCookware(1)-1,1);
                 }
                 else
                 {
-                    inv.setCookware(getCookWare(2)-1,2);
+                    inv.setCookware(inv.getCookware(2)-1,2);
                 }
             }
             else
@@ -107,17 +107,17 @@ Inventory Party:: Misfortunes()
             }
 
         }
-        else if(rand <= 40)
+        else if(randum <= 40)
         {
-            rand = rand()%2 + 1;
+            randum = rand()%2 + 1;
             //weapons
-            if(rand == 1)
+            if(randum == 1)
             {
-                rand = rand()%5;
+                randum = rand()%5;
                 // tryna go through weapons array and if i = the rand value, remove weapons[i]
-                for(int i = 0; i < 4 )
+                for(int i = 0; i < 4; i++)
                 {
-                    if(i == rand)
+                    if(i == randum)
                     {
                         cout << "Your weapon at index (add index here) broke" << endl;
                         inv.setWeapons(inv.getWeapons(i)-1,i);
@@ -131,14 +131,14 @@ Inventory Party:: Misfortunes()
                 inv.setArmor(inv.getArmor()-1);
             }
         }
-        else if(rand <= 70)
+        else if(randum <= 70)
         {
             //Food poisoning: One party member got food poisoning, causing them to lose 10 points of hunger. If this causes them to reach zero fullness, they die of
             // hunger immediately.
-            rand = rand()%5;
-            for(int i = 0; i < 5)
+            randum = rand()%5;
+            for(int i = 0; i < 5;i++)
             {
-                if(i == rand)
+                if(i == randum)
                 {
                     setFullness(getFullness(i)-1,i);
                     if(getFullness(1) <= 0)
@@ -152,10 +152,10 @@ Inventory Party:: Misfortunes()
         }
         else if((map.isRoomLocation(map.getPlayerRow(),map.getPlayerCol()) == true) && (key == 0))
         {
-            rand = rand()%4 + 1;
+            randum = rand()%4 + 1;
             for(int i = 1; i <= 4; i++)
             {
-                if(i == rand)
+                if(i == randum)
                 {
                     deathFunc(i);
                 }
